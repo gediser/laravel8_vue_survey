38e6f20ff94a7a1c6a3bbee1e7e9c45e44638b2d
Gediser <gediserdouala2021@gmail.com>
2022-03-30 11:07:57 +0200

    We can register

---
+++ vue/src/views/Surveys.vue
@@ -0,0 +1,12 @@
+<template>
+    <page-component title="Surveys">Content goes here</page-component>
+</template>
+
+<script setup>
+import PageComponent from "../components/PageComponent.vue"
+
+</script>
+
+<style scoped>
+
+</style>
