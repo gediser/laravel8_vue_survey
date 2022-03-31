2a4555541cf122acb2991320c01ff6b812cfd2b3
Gediser <gediserdouala2021@gmail.com>
2022-03-31 10:26:17 +0200

    We can logout with the server

--- vue/src/components/DefaultLayout.vue
+++ vue/src/components/DefaultLayout.vue
@@ -111,8 +111,10 @@ export default {
     const router = useRouter();
 
     function logout() {
-      store.commit('logout')
-      router.push({name: 'Login'})
+      store.dispatch('logout')
+        .then(()=>{
+          router.push({name: 'Login'})
+        })
     }
     return {
       user: computed(() => store.state.user.data),
