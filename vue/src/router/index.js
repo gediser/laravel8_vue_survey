8d6f0208df10751f5aafceb875c1538cb6815b93
Gediser <gediserdouala2021@gmail.com>
2022-03-31 12:47:36 +0200

    We can see all the surveys using temporary surveys data in store

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router"
 import  Dashboard from '../views/Dashboard.vue'
 import  Surveys from '../views/Surveys.vue'
+import SurveyView from '../views/SurveyView.vue'
 import  Login from '../views/Login.vue'
 import  Register from '../views/Register.vue'
 import  DefaultLayout from '../components/DefaultLayout.vue'
@@ -25,6 +26,16 @@ const routes = [
                 path: '/surveys',
                 name: 'Surveys',
                 component: Surveys
+            },
+            { 
+                path: '/surveys/create',
+                name: 'SurveyCreate',
+                component: SurveyView
+            },
+            { 
+                path: '/surveys:id',
+                name: 'SurveyView',
+                component: SurveyView
             }
         ]
     },
